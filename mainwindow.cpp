f4e4ba4159abf001671e9e7339c1ea3573a23067
Bardia Daneshvar <bardia.daneshvar@gmail.com>
2019-05-06 19:32:00 +0430

    Init

---
+++ mainwindow.cpp
@@ -0,0 +1,47 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+void MainWindow::on_textEdit_textChanged()
+{
+    QString result;
+    QString text = ui->textEdit->toPlainText();
+    QStringList lines = text.split("\n");
+    for(const QString &l: lines)
+    {
+        for(qint32 i=l.count()-1; i >= 0; i--)
+        {
+            QChar ch = l.at(i);
+            if(ch.direction() != QChar::DirAL && result.count() > 0)
+            {
+                qint32 lastIdx = result.count()-1;
+                while(lastIdx >= 0 && result.at(lastIdx).direction() != QChar::DirAL)
+                    lastIdx--;
+
+                qDebug() << result.count()-1 << lastIdx << ch;
+                if(lastIdx == -1)
+                    result += ch;
+                else
+                    result.insert(lastIdx+1, ch);
+            }
+            else
+                result += ch;
+
+        }
+        result += "\n";
+    }
+    ui->textBrowser->setText(result);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
